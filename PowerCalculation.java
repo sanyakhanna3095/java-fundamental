baff170d10158380fc8648d331b8f007a31120db
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-02-27 14:08:55 +0530

    [Sanya] Add: Calculate base raised to a power

--- PowerCalculation.java
+++ PowerCalculation.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
+
 public class PowerCalculation{
         public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the base number: ");
-        int x=sc.nextInt();
-        System.out.println("Enter the exponent: ");
-        int a=sc.nextInt();
+	        Scanner sc=new Scanner(System.in);
+        	System.out.println("Enter the base number: ");
+       		int x=sc.nextInt();
+        	System.out.println("Enter the exponent: ");
+        	int a=sc.nextInt();
 
-        //Calculate the number raised to power of exponent
-        double power=Math.pow(x,a);
+        	//Calculate the number raised to power of exponent
+        	double power=Math.pow(x,a);
 
-        System.out.printf("Power: "+ power);
+        	System.out.printf("Power: "+ power);
 
-}
+	}
 }
 
