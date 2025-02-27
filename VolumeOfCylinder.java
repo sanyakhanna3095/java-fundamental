0c0ab0e5b8e663849eab7e51c5cf40f1ca13e3bb
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-02-27 14:12:59 +0530

    [Sanya] Add: Calculate Volume of cylinder

--- VolumeOfCylinder.java
+++ VolumeOfCylinder.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
+
 public class VolumeOfCylinder{
-public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+        	Scanner sc = new Scanner(System.in);
+        	System.out.print("Enter the radius of the cylinder: ");
+        	double radius = sc.nextDouble();
 
-        System.out.print("Enter the radius of the cylinder: ");
-        double radius = sc.nextDouble();
+        	System.out.print("Enter the height of the cylinder: ");
+        	double height = sc.nextDouble();
 
-        System.out.print("Enter the height of the cylinder: ");
-        double height = sc.nextDouble();
+        	// Calculating the volume of the cylinder
+        	double volume = Math.PI * Math.pow(radius, 2) * height;
 
-        // Calculating the volume of the cylinder
-        double volume = Math.PI * Math.pow(radius, 2) * height;
-
-        System.out.printf("The volume of the cylinder is: %.2f cubic units%n", volume);
-    }
+        	System.out.printf("The volume of the cylinder is: %.2f cubic units%n", volume);
+    	}
 }
