ab15ae5b0586ff377d67f5a1987128e7efde6538
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-02-27 14:05:00 +0530

    [Sanya] Add: Convert kilometers to miles

--- KmToMiles.java
+++ KmToMiles.java
@@ -1,12 +1,14 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class KmToMiles{
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-System.out.print("Enter distance in kilometer: ");
-int x=sc.nextInt();
-//Convert the kilometer distance into miles
-double miles=x* 0.621371;
-System.out.printf("The distance in miles is : %.2f", miles);
-}
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter distance in kilometer: ");
+		int x=sc.nextInt();
+		
+		//Convert the kilometer distance into miles
+		double miles=x* 0.621371;
+		System.out.printf("The distance in miles is : %.2f", miles);
+	}
 }
 
